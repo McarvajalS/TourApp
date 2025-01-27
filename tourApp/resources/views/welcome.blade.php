a46d4e7cb944c604bf6c79c713a9c7ccbbf9fef6
delmonicoCL <taiker17@gmail.com>
2025-01-27 22:38:29 +0100

    hola mundo

---
+++ tourApp/resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tour App - MPV</title>
+    <!-- Incluir el CSS de Bootstrap -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+    <h1 class="mt-5 ms-3">¡Hola, Mundo :) !</h1>
+    <button class="mt-3 ms-5 btn btn-danger">Soy un boton y tù?</button>
+
+    <!-- Incluir el JS de Bootstrap -->
+    @vite('resources/js/app.js')
+</body>
+</html>
