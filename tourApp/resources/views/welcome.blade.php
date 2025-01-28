31b7ea9cac64dfc324cc0fe43ccb446a38f3aa27
delmonicoCL <taiker17@gmail.com>
2025-01-28 17:58:30 +0100

    con vendor/node_module

--- tourApp/resources/views/welcome.blade.php
+++ tourApp/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tour App - MPV</title>
     <!-- Incluir el CSS de Bootstrap -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
